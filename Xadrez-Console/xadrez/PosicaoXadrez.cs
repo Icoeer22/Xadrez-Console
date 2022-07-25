0cd8b69b808784ab4210a3658e09187a2819a0bc
Icoeer <pabloaugustodequadros@gmail.com>
2022-07-24 23:21:01 -0300

    Melhorando a impressão do Tabuleiro e implementando cor

--- Xadrez-Console/xadrez/PosicaoXadrez.cs
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -1,10 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Xadrez_Console.tabuleiro;
 
 namespace Xadrez_Console.xadrez
 {
     internal class PosicaoXadrez
     {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+
+        public PosicaoXadrez (char coluna , int linha)
+        {
+            this.Coluna = coluna;
+            this.Linha = linha;
+
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
     }
 }
